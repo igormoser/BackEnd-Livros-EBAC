36cc707a1f5a789519d0091130035e69efd2bec5
Igor <igormoser@outlook.com>
2025-08-29 18:07:32 -0300

    Adicionando função de tarefas ao código!

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-from typing import Optional
-app = FastAPI()
+from typing import List, Optional
+
+app = FastAPI(
+    title = "API de Livros",
+    description = "Catálogo de Livros",
+    version = "0.0.1",
+    contact ={"name":"Igor Araujo",
+              "email":"igormoser@outlook.com"})
 
 lista_livros = {}
+tarefas: List[Tarefa] = []
+
+class Tarefa(BaseModel):
+    nome: str
+    descricao: str
+    concluida: Optional[bool] = False
 
 class Livro(BaseModel):
     nome_livro: str
     autor_livro: str
     ano_livro: int
+
+@app.get("/tarefas")
+def listar_tarefas():
+    return tarefas
 
 @app.get("/livros")
 def get_livros():
@@ -16,6 +32,11 @@ def get_livros():
         return {"Este livro não existe na lista!"}
     else:
         return {"Livros": lista_livros}
+@app.post("/tarefas")
+def adicionar_tarefas(tarefa: Tarefa):
+    tarefas.append(tarefa)
+    return {"mensagem": "Tarefa adicionada com sucesso!", "tarefa": tarefa}
+
 @app.post("/adicionar")
 def post_livros(id_livro: int, livro: Livro):
     if id_livro in lista_livros:
@@ -23,6 +44,14 @@ def post_livros(id_livro: int, livro: Livro):
     else:
         lista_livros[id_livro] = livro.model_dump()
         return {"message": "O livro foi adicionado com sucesso!"}
+@app.put("/tarefas/{nome}")
+def concluir_tarefa(nome: str):
+    for tarefa in tarefas:
+        if tarefa.nome == nome:
+            tarefa.concluida = True
+            return {"mensagem":"Tarefa concluida com sucesso!", "tarefa": tarefa}
+    return {"Erro": "Tarefa não encontrada!"}
+
 
 @app.put("/atualizar/{id_livro}")
 def put_livros(id_livro: int, livro: Livro):
@@ -32,8 +61,15 @@ def put_livros(id_livro: int, livro: Livro):
     else:
         lista_livros[id_livro] = livro.model_dump()
         return {"message": "O livro foi atualizado com sucesso!"}
+@app.delete("/tarefas/{nome}")
+def remover_tarefas(nome: str):
+    for tarefa in tarefas:
+        if tarefa.nome == nome:
+            tarefa.remove(tarefa)
+            return {"message": "Tarefa removida com sucesso!", "tarefa": tarefa}
+    return {"Erro": "Tarefa não encontrada"}
 
-@app.delete("/deletar/{id_livro}")
+ @app.delete("/deletar/{id_livro}")
 def delete_livros(id_livro: int):
     if id_livro not in lista_livros:
         raise HTTPException(status_code=404, detail="Este livro não foi encontrado!")
